660c6c36fd6667dc398d164f95bac49ee4c46330
Raleigh.Luo <Raleigh.Luo@live.com>
2017-06-27 16:37:32 +0800

    主页动画 调整2

--- app/src/main/java/com/gkzxhn/wisdom/MainActivity.java
+++ app/src/main/java/com/gkzxhn/wisdom/MainActivity.java
@@ -8,10 +8,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.gkzxhn.wisdom.adapter.NoticeAdapter;
 
@@ -41,13 +44,17 @@ public class MainActivity extends AppCompatActivity {
     NestedScrollView mNestedScrollView; // 标题栏Title
     @Bind(R.id.main_layout_recyclerView)
     RecyclerView mRecyclerView; // 工具栏
+    @Bind(R.id.main_layout_toolbar)
+    Toolbar mToolbar; // 工具栏
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-
+        mToolbar.setTitle("");
+        setSupportActionBar(mToolbar);
         // AppBar的监听
         mAblAppBar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
             @Override
@@ -63,6 +70,13 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(new NoticeAdapter(this));
         mNestedScrollView.setFillViewport(true);//NestedScrollView子项全屏
 //        mRecyclerView.setNestedScrollingEnabled(false);//解决滑动不流畅的问题
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(getApplicationContext(),"Click Setting",Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
     private boolean isExpanded=true;
     public void onClickListener(View view){
@@ -104,5 +118,10 @@ public class MainActivity extends AppCompatActivity {
         alphaAnimation.setDuration(duration);
         alphaAnimation.setFillAfter(true);
         v.startAnimation(alphaAnimation);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
--- app/src/main/java/com/gkzxhn/wisdom/behavior/AvatarImageBehavior.java
+++ app/src/main/java/com/gkzxhn/wisdom/behavior/AvatarImageBehavior.java
@@ -1,8 +1,10 @@
 package com.gkzxhn.wisdom.behavior;
 
 import android.content.Context;
+import android.graphics.Point;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -12,6 +14,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.gkzxhn.wisdom.R;
+import com.starlight.mobile.android.lib.util.ViewUtil;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -31,11 +34,9 @@ public class AvatarImageBehavior extends CoordinatorLayout.Behavior<RelativeLayo
     private int mFinalHeight; // 结束的图片高度
     private int mStartXPosition; // 起始的X轴高度
     private int mFinalXPosition; // 结束的X轴高度
-    private float mStartToolbarPosition; // Toolbar的起始位置
 
     private final Context mContext;
     private float mAvatarMaxSize;
-    private int mImageStartMargin,mImageFinalMargin;
 
     public AvatarImageBehavior(Context context, AttributeSet attrs) {
         mContext = context;
@@ -48,15 +49,14 @@ public class AvatarImageBehavior extends CoordinatorLayout.Behavior<RelativeLayo
 
     private void bindDimensions() {
         mAvatarMaxSize = mContext.getResources().getDimension(R.dimen.image_width);
-        mImageStartMargin =mContext.getResources().getDimensionPixelSize(R.dimen.image_start_margin);
-        mImageFinalMargin =mContext.getResources().getDimensionPixelSize(R.dimen.image_final_margin);
+
     }
 
 
     @Override
     public boolean layoutDependsOn(CoordinatorLayout parent, RelativeLayout child, View dependency) {
         // 依赖Toolbar控件
-        return dependency instanceof Toolbar;
+        return dependency instanceof NestedScrollView;
     }
 
     @Override
@@ -68,29 +68,45 @@ public class AvatarImageBehavior extends CoordinatorLayout.Behavior<RelativeLayo
 
         // 最大滑动距离: 起始位置-状态栏高度
 //        final int maxScrollDistance = (int) (mStartToolbarPosition - getStatusBarHeight());
-        final int  maxScrollDistance=getStatusBarHeight();
+        Point point=new Point();
+        ViewUtil.getScreenDisplay(mContext).getSize(point);
+        final int  maxScrollDistance=point.x;//获取屏幕的高度  NestedScrollView滑动的区域是整个屏幕
         // 滑动的百分比
         float expandedPercentageFactor = dependency.getY() / maxScrollDistance;
+        if(expandedPercentageFactor>1)expandedPercentageFactor=1;
 
         for(int i=1;i<childRoot.getChildCount();i++){
+            childRoot.getChildAt(i);
             TextView tvTitle= (TextView) childRoot.getChildAt(i);
             int dimen=0;
+            float disSize=0;
+            int padingTop=0;
             switch (tvTitle.getId()){
                 case R.id.home_full_screen_layout_tv_week:
                 case R.id.home_full_screen_layout_tv_date:
                     dimen=R.dimen.main_layout_date_text_size;
+                    padingTop=mContext.getResources().getDimensionPixelSize(
+                            R.dimen.main_layout_date_padding_top);
+                    disSize=5*(1-expandedPercentageFactor);//[0-5的区间]
                     break;
                 case R.id.home_full_screen_layout_tv_home_number:
                     dimen=R.dimen.main_layout_home_number_text_size;
+                    padingTop=mContext.getResources().getDimensionPixelSize(
+                            R.dimen.main_layout_home_number_padding_top);
+                    disSize=25*(1-expandedPercentageFactor);//[0-25的区间]
                     break;
                 case R.id.home_full_screen_layout_tv_community_name:
+                    padingTop=mContext.getResources().getDimensionPixelSize(
+                            R.dimen.main_layout_community_name_padding_top);
                     dimen=R.dimen.main_layout_community_name_text_size;
+                    disSize=30*(1-expandedPercentageFactor);//区间 expandedPercentageFactor［0，1］换[0-30的区间]
                     break;
 
             }
             int titleSize=mContext.getResources().getDimensionPixelSize(dimen);
-            float disSize=4*(1-expandedPercentageFactor);//[0-4的区间]
-            tvTitle.setTextSize(TypedValue.COMPLEX_UNIT_PX, (float) (titleSize-disSize));
+            tvTitle.setTextSize(TypedValue.COMPLEX_UNIT_PX, titleSize-disSize);
+            //区间 expandedPercentageFactor［0，1］换区间［0.3-1］
+            tvTitle.setPadding(0, (int) (padingTop*(0.7*expandedPercentageFactor+0.3)),0,0);
         }
 //        Log.e("mStartToolbarPosition="+mStartToolbarPosition,"raleigh_test");
 //        Log.e("getStatusBarHeight()="+getStatusBarHeight(),"raleigh_test");
@@ -104,14 +120,6 @@ public class AvatarImageBehavior extends CoordinatorLayout.Behavior<RelativeLayo
         // X轴距离
         float distanceXToSubtract = ((mStartXPosition - mFinalXPosition)
                 * (1f - expandedPercentageFactor)) + dis;
-
-        // Y轴距离
-//        float distanceYToSubtract = ((mStartYPosition - mFinalYPosition)
-//                * (1f - expandedPercentageFactor)) + (child.getHeight() / 2);
-//
-//        // X轴距离
-//        float distanceXToSubtract = ((mStartXPosition - mFinalXPosition)
-//                * (1f - expandedPercentageFactor)) + (child.getWidth() / 2);
 
 //        Log.e("distanceXToSubtract="+distanceXToSubtract,"raleigh_test");
 //        Log.e("distanceYToSubtract="+distanceYToSubtract,"raleigh_test");
@@ -147,11 +155,12 @@ public class AvatarImageBehavior extends CoordinatorLayout.Behavior<RelativeLayo
     private void shouldInitProperties( RelativeLayout childRoot,CircleImageView child, View dependency) {
 //        Log.e("childX="+child.getX()+",childY"+ child.getY()+",childHeigth"+child.getHeight(),"raleigh_test");
 //        Log.e("dependencyX="+dependency.getX()+",dependencyY"+ dependency.getY()+",dependencyHeigth"+dependency.getHeight(),"raleigh_test");
-
+        int imageMargrinTop=childRoot.getPaddingTop()+mContext.getResources().getDimensionPixelSize(R.dimen.image_start_margin);
+        int imageMargrinLeft=childRoot.getPaddingLeft()+mContext.getResources().getDimensionPixelSize(R.dimen.image_start_margin);
         // 图片控件中心
         if (mStartYPosition == 0)
 //            mStartYPosition = (int) (child.getY() + (child.getHeight() / 2));
-            mStartYPosition = (int) (child.getY()> mImageStartMargin ?child.getY(): mImageStartMargin + (child.getHeight() / 2));
+            mStartYPosition = (int) (child.getY()> imageMargrinTop ?child.getY(): imageMargrinTop + (child.getHeight() / 2));
         // Toolbar中心
 
         // 图片高度
@@ -164,30 +173,12 @@ public class AvatarImageBehavior extends CoordinatorLayout.Behavior<RelativeLayo
 
         // 图片控件水平中心
         if (mStartXPosition == 0)
-//            mStartXPosition = (int) (child.getX() + (child.getWidth() / 2));
-            mStartXPosition = (int) (child.getX()> mImageStartMargin ?child.getX(): mImageStartMargin + (child.getWidth() / 2));
+            mStartXPosition = (int) (child.getX()> imageMargrinLeft ?child.getX(): imageMargrinLeft + (child.getWidth() / 2));
 
         // 边缘+缩略图宽度的一半
         if (mFinalXPosition == 0)
             mFinalXPosition=childRoot.getPaddingLeft()+(mFinalHeight / 2);
-//            mFinalXPosition = mContext.getResources().getDimensionPixelOffset(R.dimen.image_final_margin) + (mFinalHeight / 2);
         if (mFinalYPosition == 0)
             mFinalYPosition = childRoot.getPaddingTop()+(mFinalHeight / 2);
-
-        // Toolbar的起始位置
-        if (mStartToolbarPosition == 0)
-            mStartToolbarPosition = dependency.getY() + (dependency.getHeight() / 2);
-    }
-
-    // 获取状态栏高度
-    public int getStatusBarHeight() {
-        int result = 0;
-        result=mContext.getResources().getDimensionPixelSize(R.dimen.toolbar_height);
-//        int resourceId = mContext.getResources().getIdentifier("status_bar_height", "dimen", "android");
-//
-//        if (resourceId > 0) {
-//            result = mContext.getResources().getDimensionPixelSize(resourceId);
-//        }
-        return result;
     }
 }
