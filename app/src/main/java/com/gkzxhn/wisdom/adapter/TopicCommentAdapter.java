21494a4fd98d92f9d861ac6bd6ab90654d0ebcfa
Raleigh.Luo <Raleigh.Luo@live.com>
2017-07-14 10:21:13 +0800

    话题详情UI

--- app/src/main/java/com/gkzxhn/wisdom/adapter/TopicCommentAdapter.java
+++ app/src/main/java/com/gkzxhn/wisdom/adapter/TopicCommentAdapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.gkzxhn.wisdom.R;
 
@@ -32,13 +34,23 @@ public class TopicCommentAdapter extends RecyclerView.Adapter<TopicCommentAdapte
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 10;
     }
 
     class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView tvName,tvDate,tvContent,tvLikeNumber;
+        private ImageView ivPortrait,ivLike,ivComment;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            tvName= (TextView) itemView.findViewById(R.id.topic_comment_layout_tv_name);
+            tvDate= (TextView) itemView.findViewById(R.id.topic_comment_layout_tv_date);
+            tvContent= (TextView) itemView.findViewById(R.id.topic_comment_layout_tv_content);
+            tvLikeNumber= (TextView) itemView.findViewById(R.id.topic_comment_layout_tv_like);
+            ivPortrait= (ImageView) itemView.findViewById(R.id.topic_comment_layout_iv_portrait);
+            ivLike= (ImageView) itemView.findViewById(R.id.topic_comment_layout_iv_date);
+            ivComment= (ImageView) itemView.findViewById(R.id.topic_comment_layout_iv_portrait);
+
         }
     }
 }
