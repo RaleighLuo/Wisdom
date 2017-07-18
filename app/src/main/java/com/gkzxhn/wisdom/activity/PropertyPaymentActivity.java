3bcc44cf01d8a9f72a35b4eaf4007614afe3921a
Raleigh.Luo <Raleigh.Luo@live.com>
2017-07-18 16:10:11 +0800

    支付详情

--- app/src/main/java/com/gkzxhn/wisdom/activity/PropertyPaymentActivity.java
+++ app/src/main/java/com/gkzxhn/wisdom/activity/PropertyPaymentActivity.java
@@ -2,10 +2,16 @@ package com.gkzxhn.wisdom.activity;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.gkzxhn.wisdom.R;
 import com.gkzxhn.wisdom.customview.PropertyPaymentDialog;
+
+import static com.gkzxhn.wisdom.R.id.property_payment_layout_v_community;
 
 /**
  * Created by Raleigh.Luo on 17/7/18.
@@ -14,6 +20,10 @@ import com.gkzxhn.wisdom.customview.PropertyPaymentDialog;
 
 public class PropertyPaymentActivity extends SuperActivity{
     private PropertyPaymentDialog mPropertyPaymentDialog;
+    private Button btnPay;
+    private TextView tvPleasePay;
+    private RadioGroup mRadioGroup;
+    private int mLastCheckId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,19 +32,31 @@ public class PropertyPaymentActivity extends SuperActivity{
         init();
     }
     private void initControls(){
-
+        tvPleasePay= (TextView) findViewById(R.id.property_payment_layout_tv_please_pay);
+        btnPay= (Button) findViewById(R.id.property_payment_layout_btn_bottom);
+        mRadioGroup= (RadioGroup) findViewById(R.id.property_payment_layout_rg_month);
     }
     private void init(){
+        mRadioGroup.check(R.id.property_payment_layout_rb_first);
         mPropertyPaymentDialog=new PropertyPaymentDialog(this);
         mPropertyPaymentDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 switch (v.getId()){
                     case R.id.property_payment_dialog_layout_tv_alipay://支付宝支付
                         break;
                     case R.id.property_payment_dialog_layout_tv_weixin://微信支付
                         break;
                 }
+
+                tvPleasePay.setText(R.string.has_pay);
+                btnPay.setVisibility(View.INVISIBLE);
+                findViewById(R.id.property_payment_layout_tv_pay_success).setVisibility(View.VISIBLE);
+                if(mLastCheckId!=R.id.property_payment_layout_rb_first)findViewById(R.id.property_payment_layout_rb_first).setEnabled(false);
+                if(mLastCheckId!=R.id.property_payment_layout_rb_second)findViewById(R.id.property_payment_layout_rb_second).setEnabled(false);
+                if(mLastCheckId!=R.id.property_payment_layout_rb_third)findViewById(R.id.property_payment_layout_rb_third).setEnabled(false);
+
             }
         });
     }
@@ -44,6 +66,7 @@ public class PropertyPaymentActivity extends SuperActivity{
                 finish();
                 break;
             case R.id.property_payment_layout_btn_bottom://支付
+                mLastCheckId=mRadioGroup.getCheckedRadioButtonId();
                 if(mPropertyPaymentDialog!=null&&!mPropertyPaymentDialog.isShowing())mPropertyPaymentDialog.show();
                 break;
         }
