f6b31ff44f14015c56ad627c7293eb5c26083ac0
Raleigh.Luo <Raleigh.Luo@live.com>
2017-07-18 13:15:11 +0800

    报修进度

---
+++ app/src/main/java/com/gkzxhn/wisdom/activity/PropertyPaymentActivity.java
@@ -0,0 +1,18 @@
+package com.gkzxhn.wisdom.activity;
+
+import android.os.Bundle;
+
+import com.gkzxhn.wisdom.R;
+
+/**
+ * Created by Raleigh.Luo on 17/7/18.
+ * 支付物业费
+ */
+
+public class PropertyPaymentActivity extends SuperActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.property_payment_layout);
+    }
+}
