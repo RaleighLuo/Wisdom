e06a78673c226f063e79c4dce07b2f00dae9e178
Raleigh.Luo <Raleigh.Luo@live.com>
2017-07-18 11:17:34 +0800

    报修进度

---
+++ app/src/main/java/com/gkzxhn/wisdom/activity/RepairProgressActivity.java
@@ -0,0 +1,28 @@
+package com.gkzxhn.wisdom.activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.gkzxhn.wisdom.R;
+
+/**
+ * Created by Raleigh.Luo on 17/7/18.
+ * 报修进度
+ */
+
+public class RepairProgressActivity extends SuperActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.repair_progress_layout);
+    }
+    public void onClickListener(View view){
+        switch (view.getId()){
+            case R.id.repair_progress_layout_iv_back:
+                finish();
+                break;
+            case R.id.repair_progress_layout_iv_award:
+                break;
+        }
+    }
+}
--- app/src/main/java/com/gkzxhn/wisdom/adapter/RepairAdapter.java
+++ app/src/main/java/com/gkzxhn/wisdom/adapter/RepairAdapter.java
@@ -1,12 +1,14 @@
 package com.gkzxhn.wisdom.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.gkzxhn.wisdom.R;
+import com.gkzxhn.wisdom.activity.RepairProgressActivity;
 
 /**
  * Created by Raleigh.Luo on 17/7/11.
@@ -28,6 +30,12 @@ public class RepairAdapter extends RecyclerView.Adapter<RepairAdapter.ViewHolder
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                context.startActivity(new Intent(context,RepairProgressActivity.class));
+            }
+        });
 
     }
 
